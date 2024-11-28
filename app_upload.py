6ec91d9db57b4626434830c4168f486aab5612d7
mariamadial <92579883+mariamadial@users.noreply.github.com>
2024-11-28 18:49:26 +0000

    app_upload.py

--- app_upload.py
+++ app_upload.py
@@ -2,7 +2,7 @@
 #import seaborn as sns
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import streamlit as st
 import base64
 import plotly.express as px
